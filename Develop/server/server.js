86c09dfd40685b761cfd8851e5cd75df923a2a3d
Kole Lam <kolelamied1@gmail.com>
2023-08-17 16:12:23 -0600

    bug fixes/reinstall node modules

--- Develop/server/server.js
+++ Develop/server/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
@@ -6,10 +7,36 @@ const routes = require('./routes');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// Import your GraphQL type definitions and resolvers here
+const typeDefs = `
+  type Query {
+    hello: String
+  }`;
+
+const resolvers = {
+  Query: {
+    hello: () => "Hello, World!",
+  },
+};
+
+// Creates Apollo Server
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+// Apply Apollo Server as middleware to Express js
+async function startApolloServer() {
+  await server.start();
+  server.applyMiddleware({ app });
+}
+
+startApolloServer();
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// if we're in production, serve client/build as static assets
+// if in production, serve build as static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
