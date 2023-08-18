c6de6b24456f28b65748b6a84c9ccc262a5bf657
Kole Lam <kolelamied1@gmail.com>
2023-08-17 18:17:12 -0600

    heroku deploy fix

--- server/config/connection.js
+++ server/config/connection.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 // Connecting to the MongoDB using Mongoose
 // The 'process.env.MONGODB_URI' allows connection to a remote DB specified in an env (like on a deployment platform)
 // If 'process.env.MONGODB_URI' is not set, it will use a local MongoDB instance at 'mongodb://localhost/googlebooks'
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks', {
   useNewUrlParser: true,        // Parse connection strings using the MongoDB driver's URL parser
   useUnifiedTopology: true,    // Use the new MongoDB driver's engine
 });
